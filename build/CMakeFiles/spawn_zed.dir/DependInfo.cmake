86f3eac6c8da83e553486c1b0745eb2efab775a9
Farid <faridsahebsara@gmail.com>
2024-06-28 16:56:49 -0500

    ?

---
+++ build/CMakeFiles/spawn_zed.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/icore/ros2_ws/src/box/src/spawn_zed.cpp" "CMakeFiles/spawn_zed.dir/src/spawn_zed.cpp.o" "gcc" "CMakeFiles/spawn_zed.dir/src/spawn_zed.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/spawn_zed.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spawn_zed.dir/src/spawn_zed.cpp.o"
+  "CMakeFiles/spawn_zed.dir/src/spawn_zed.cpp.o.d"
+  "spawn_zed"
+  "spawn_zed.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spawn_zed.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
